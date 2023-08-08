b911c4867397f007b1bf260ee3c5b7f043160428
Laura Feugene <laura.feugene.gmail.com>
2023-08-08 15:16:27 +0200

    feat: :sparkles: ajout de la map leaflet

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Map from './Components/Map/Map'
 import SearchForm from './Components/SearchForm/SearchForm'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
     <SearchForm />
+    <Map />
     </>
   )
 }
--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,1 +1,27 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import './Map.css'
+
+function Map() {
+ 
+  return (
+    <>
+    <div id="map"></div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{
+    position: 'relative',
+    height: 500
+  }}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+    </>
+  )
+}
+
+export default Map;
