bf8d8f04041efb45fa93d7675c83e87f158902fb
Laura Feugene <laura.feugene.gmail.com>
2023-08-09 16:55:00 +0200

    feat: :sparkles: connexion à api ip geolocalisation

--- src/Components/ResultForm/ResultForm.jsx
+++ src/Components/ResultForm/ResultForm.jsx
@@ -1,20 +1,36 @@
+import { useEffect, useState } from 'react';
 import './ResultForm.css'
+import {client} from '../../api/api';
 
 function ResultForm() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        client.get("", {
+            params: {
+                apiKey: "at_6Ew9bBFZfU5khd8IKOvOTARMLhzdP",
+                // ipAddress: "8.8.8.8",
+            },
+        }).then((response) => {
+            setData(response.data);
+        });
+    }, []);
+    
+    if (!data) return (<div>Loading...</div>);
     
       return (
      <div className="resultform">
         <article><h3>IP Address</h3>
-            <p>coucou</p>
+            <p>{data.ip}</p>
         </article>
         <article><h3>Localisation</h3>
-            <p>coucou</p>
+            <p>{data.location.city}</p>
         </article>
         <article><h3>Timezone</h3>
-            <p>coucou</p>
+            <p>{data.location.timezone}</p>
         </article>
         <article><h3>ISP</h3>
-            <p>coucou</p>
+            <p>{data.isp}</p>
         </article>
      </div>
       )
