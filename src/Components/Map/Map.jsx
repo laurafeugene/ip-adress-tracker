6c227334d7b233109f37c8fd0be9c8664348d426
Laura Feugene <laura.feugene.gmail.com>
2023-08-21 15:17:25 +0200

    feat: tout fonctionne

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -3,13 +3,13 @@ import LocationMarker from './LocationMarker';
 
 
 function CustomMap({data}){
-  if (!data) return (<div>Enter an IP Address...</div>);
+if (!data) return (<div>Enter an IP Address...</div>);
 const position = [Number(data.location.lat), Number(data.location.lng)];
 console.log(position);
 
   return (
     <div id="map">
-     <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{position:"relative", height: 500, zIndex:1}}>
+     <MapContainer center={position} zoom={15} scrollWheelZoom={true} style={{position:"relative", height: 500, zIndex:1}}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
